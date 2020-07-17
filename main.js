f40a7e77ae7b6639bfb3ca1b31da68ca56113258
Chad <chdabney42@gmail.com>
2020-07-17 14:33:26 -0400

    stopped it from adding more than 6

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
+// query selectors
 const columnContainer = document.querySelector('.columnContainer')
 const columns = document.querySelectorAll('.column')
 
 
-let currentPlayer = 0
+let board = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+]
 
+
+//track turn
+let currentPlayer = 1
+
+//for loop to add clicker event to each column and add  red or black chip
+let eventColumn
 for (let index = 0; index < columns.length; index++) {
     const currentColumn = columns[index];
     currentColumn.addEventListener('click', function () {
         let chip = document.createElement('div')
-        if (currentPlayer === 0) {
-            chip.setAttribute('class', 'redChip')
-            currentPlayer = 1
+        eventColumn = event.currentTarget
+        if (eventColumn.children.length === 6) {
+            alert('nope')
         } else if (currentPlayer === 1) {
+            chip.setAttribute('class', 'redChip')
+            currentPlayer = 2
+            currentColumn.append(chip)
+        } else if (currentPlayer === 2) {
             chip.setAttribute('class', 'blackChip')
-            currentPlayer = 0
-
+            currentPlayer = 1
+            currentColumn.append(chip)
         }
-        currentColumn.append(chip)
+
     })
 }
